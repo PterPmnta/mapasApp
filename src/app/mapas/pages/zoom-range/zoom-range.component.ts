51b7e8182dbe88ce86c11e32bdaed110a390d05a
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-07-28 17:24:59 -0500

    add range component and buttons

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .row {
+        background-color: white;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        border-radius: 5px;
+        z-index: 999;
+      }
+    `,
+  ],
 })
 export class ZoomRangeComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-72.25323448337556, 11.373452774112943],
+      zoom: 17,
+    });
   }
-
 }
