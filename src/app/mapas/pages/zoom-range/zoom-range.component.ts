2eeddd04df2bf3176a17aab3e820e19b11c27e4f
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-08-04 15:32:01 -0500

    get geo-data and other changes

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -2,7 +2,7 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
-  OnInit,
+  OnDestroy,
   ViewChild,
 } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
@@ -29,18 +29,25 @@ import * as mapboxgl from 'mapbox-gl';
     `,
   ],
 })
-export class ZoomRangeComponent implements AfterViewInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 13;
+  geodata: [number, number] = [-72.25323448337556, 11.373452774112943];
 
   constructor() {}
+
+  ngOnDestroy(): void {
+    this.mapa.off('zoom', () => {});
+    this.mapa.off('zoomend', () => {});
+    this.mapa.off('move', () => {});
+  }
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-72.25323448337556, 11.373452774112943],
+      center: this.geodata,
       zoom: this.zoomLevel,
     });
 
@@ -52,6 +59,12 @@ export class ZoomRangeComponent implements AfterViewInit {
       if (this.mapa.getZoom() > 18) {
         this.mapa.zoomTo(18);
       }
+    });
+
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.geodata = [lng, lat];
     });
   }
 
