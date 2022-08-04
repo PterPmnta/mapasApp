d023f9b91eb9de3acb08ab58a070c0fd8f1e8a7e
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-08-04 17:16:25 -0500

    marker added to geo-data stablesh

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
-  ]
+    `
+      .mapa-container {
+        width: 100%;
+        height: 100%;
+      }
+    `,
+  ],
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 13;
+  geodata: [number, number] = [-72.25323448337556, 11.373452774112943];
+  constructor() {}
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.geodata,
+      zoom: this.zoomLevel,
+    });
 
-  constructor() { }
-
-  ngOnInit(): void {
+    const marker = new mapboxgl.Marker()
+      .setLngLat(this.geodata)
+      .addTo(this.mapa);
   }
-
 }
