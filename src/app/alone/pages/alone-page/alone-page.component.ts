7a8776c6ebfecf225a3024aa4afd9a287d67b449
Pedro Pimienta M <pedro.pimienta.morales@gmail.com>
2024-06-29 16:39:28 -0500

    clase 309: standalone -  como componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,14 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CounterAloneComponent } from '../../components/counter-alone/counter-alone.component';
 
 @Component({
   /* selector: 'app-alone-page', */
   standalone: true,
-  /* imports: [
+  imports: [
     CommonModule,
-  ], */
+    CounterAloneComponent
+  ],
   templateUrl: './alone-page.component.html',
   styleUrls: ['./alone-page.component.css'],
-  /* changeDetection: ChangeDetectionStrategy.OnPush, */
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlonePageComponent { }
